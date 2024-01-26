a5a90f1b0c8b4245721b4f977d82d3b0668b55da
sandeep <sandeepypb@gmail.com>
2024-01-26 14:09:00 +0530

    ✨ Feat: apollo-client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { useQuery } from "@apollo/client";
+import { GET_BOOKS } from "./graphql/queries";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const { loading, data, error } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <ul>
+      {data.books.map((book) => (
+        <li key={book.title}>
+          Title: {book.title}, Author: {book.author}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
